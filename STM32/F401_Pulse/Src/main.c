20c721cd6cef78fcfc297efee1a8ef941b0d1774
HanielUlises <hanielulises2003@gmail.com>
2026-07-27 09:34:57 -0600

    Registers for GPIOA and RCC

--- STM32/F401_Pulse/Src/main.c
+++ STM32/F401_Pulse/Src/main.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define PERIPH_BASE           (0x40000000UL)
 #define AHB1PERIPH_OFFSET     (0x00020000UL)
 #define AHB1PERIPH_BASE       (PERIPH_BASE + AHB1PERIPH_OFFSET)
@@ -21,19 +23,51 @@
 #define PIN5                  (1U << 5)
 #define LED_PIN               PIN5
 
+#define __IO volatile
+
+//typedef struct {
+//	 __IO uint32_t MODER;
+//	 __IO uint32_t OTYPER;
+//	 __IO uint32_t OSPEEDR;
+//	 __IO uint32_t PUPDR;
+//	 __IO uint32_t IDR;
+//	 __IO uint32_t ODR;
+//	 __IO uint32_t BSRR;
+//	 __IO uint32_t LCKR;
+//	 __IO uint32_t AFR[2];
+//} GPIO_Typedef;
+
+typedef struct {
+	uint32_t DUMMY[12];
+	__IO uint32_t AHB1ENR;
+} RCC_TypeDef;
+
+typedef struct {
+	__IO uint32_t MODER;
+	__IO uint32_t DUMMY[4];
+	__IO uint32_t ODR;
+} GPIO_TypeDef;
+
+#define RCC 			((RCC_TypeDef*) RCC_BASE)
+#define GPIOA 			((GPIO_TypeDef*) GPIOA_BASE)
+
 int main(void)
 {
+
     // Enable clock access to GPIOA
-    RCC_AHB1EN_R |= GPIOAEN;
+    // RCC_AHB1EN_R |= GPIOAEN;
+	RCC -> AHB1ENR |= GPIOAEN;
 
     // Set PA5 as output pin (MODER5 = 01)
-    GPIOA_MODE_R |=  (1U << 10);   // set bit 10
-    GPIOA_MODE_R &= ~(1U << 11);   // clear bit 11
+	// GPIOA_MODE_R |=  (1U << 10);   // set bit 10
+	// GPIOA_MODE_R &= ~(1U << 11);   // clear bit 11
+	GPIOA -> MODER |=  (1U << 10);   // set bit 10
+	GPIOA -> MODER &= ~(1U << 11);   // clear bit 11
 
     while (1)
     {
         // Toggle PA5
-        GPIOA_OD_R ^= LED_PIN;
+    	GPIOA -> ODR ^= LED_PIN;
         for(int i =0; i < 100000; i++) {
 
         }
