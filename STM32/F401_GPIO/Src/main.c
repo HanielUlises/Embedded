ab04aff74dadd9cef2ff400346add6f9434f88e1
HanielUlises <hanielulises2003@gmail.com>
2026-07-27 10:56:38 -0600

    Mapping to registers using stm32cubef4 drivers for GPIO

--- STM32/F401_GPIO/Src/main.c
+++ STM32/F401_GPIO/Src/main.c
@@ -1,16 +1,36 @@
 #include "stm32f4xx.h"
 
 #define GPIOAEN			(1U<<0)
+#define GPIOCEN 		(1U<<2)
+
 #define PIN5			(1U<<5)
+#define PIN13			(1u<<13)
+
 #define LED_PIN			PIN5
+#define BTN_PIN 		PIN13
 
  int main(void) {
+	 // Enable clock access to GPIOA and GPIOC
 	 RCC -> AHB1ENR |= GPIOAEN;
+	 RCC -> AHB1ENR |= GPIOCEN;
+
+	 // Set PA5 as output pin
 	 GPIOA-> MODER |= (1U << 10);
 	 GPIOA -> MODER &= ~(1U << 11);
+
+	 // Set PC13 as input pin
+	 GPIOC -> MODER &=~(1U<<26);
+	 GPIOC -> MODER &=~(1U<<27);
+
 	 while(1) {
-		 GPIOA-> ODR ^= LED_PIN;
-		 for(int i = 0; i < 100000; i++) {}
+		 // BTN pressed
+		 if(GPIOC -> IDR & BTN_PIN) {
+			 GPIOA-> ODR = LED_PIN;
+			 // for(int i = 0; i < 100000; i++) {}
+		 } else {
+			 GPIOA-> ODR = (1U<<21);
+		 }
+		 // for(int i = 0; i < 100000; i++) {}
 	 }
  }
 
