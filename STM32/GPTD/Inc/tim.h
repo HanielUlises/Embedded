46e24dcfd6b7b066cc192c29eef59eec748e5041
HanielUlises <hanielulises2003@gmail.com>
2026-08-22 18:28:19 -0600

    Timer input capture driver

--- STM32/GPTD/Inc/tim.h
+++ STM32/GPTD/Inc/tim.h
@@ -3,7 +3,9 @@
 
 void tim2_1hz_init(void);
 void tim2_pa5_output_compare(void);
+void tim3_pa6_input_capture(void);
 
 #define SR_UIF 			(1U << 0)
+#define SR_CC11F		(1U << 1)
 
 #endif
--- STM32/GPTD/Src/main.c
+++ STM32/GPTD/Src/main.c
@@ -13,8 +13,18 @@ uint32_t sensor_value;
 
 #define LED_PIN					PIN5 */
 
+int timestamp = 0;
+
 int main(void) {
 	tim2_pa5_output_compare();
+	tim3_pa6_input_capture();
+
+	while(1) {
+		// Wait until edge is captured
+		while(!(TIM3 -> SR  & SR_CC1IF)) {}
+		// Read captured value
+		timestamp = TIM3 -> CCR1;
+	}
 	/*
 	RCC -> AHB1ENR |= GPIOAEN;
 	GPIOA -> MODER |= (1U << 10);
